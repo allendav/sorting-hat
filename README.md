aeb5623b020b2575e003ea2c3098ec7c3ff4952f
Allen Snook <allen@allendav.com>
2016-05-27 15:29:16 -0700

    Process multiple repositories and return the combined results

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # urban-chainsaw
 
-Based on https://medium.com/@tribou/serving-react-and-flux-with-hapi-and-webpack-213afacf94ea#.qsfm03gpm
-and https://github.com/tribou/react-todo
+Urban Chainsaw supports displaying issues by assignee for the automattic woocommerce-connect-client repository
+by default. To view a different repository, copy `config/default.json` to `config/local.json` and edit
+that file. If the repository is private, you will have to obtain a repo API token for your GitHub user
+and save it in that file as well.
 
-`npm run dev`
+Before you run the app for the first time, simply type
+
+```
+npm install
+```
+
+And then, thereafter just do
+
+```
+npm run dev
+```
+
+And open your browser to `localhost:8000` and see all the open issues by assignee
--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
 import githubapi from 'github';
+import config from 'config';
+import async from 'async';
 
 module.exports = {
 	issues: function( request, reply ) {
@@ -13,20 +15,31 @@ module.exports = {
 			}
 		} );
 
-		// github.user.getFollowingFromUser( {
-		// 	user: "allendav"
-		// }, function(err, res) {
-		// 	reply( JSON.stringify( res ) );
-		// } );
+		const token = config.get( 'github.token' );
+		const repositories = config.get( 'github.repositories' );
 
-		github.issues.repoIssues( {
-			user: "automattic",
-			repo: "woocommerce-connect-client",
-			state: "open",
-			page: 0,
-			per_page: 100
-		}, function( err, res ) {
-			reply( res );
+		async.map( repositories, function( repository, callback ) {
+			if ( token.length ) {
+				github.authenticate( {
+					type: 'token',
+					token: token
+				} );
+			}
+
+			github.issues.repoIssues( {
+				user: repository.user,
+				repo: repository.repo,
+				state: "open",
+				page: 0,
+				per_page: 100
+			}, function( err, res ) {
+				// TODO handle errors better
+				callback( err, res );
+			} );
+
+		}, function( error, results ) {
+			const merged = [].concat.apply( [], results );
+			reply( merged );
 		} );
 
 	}
