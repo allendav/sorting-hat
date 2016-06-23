c0803f28ad63eed441a83726f91e0408cafba25f
Allen Snook <allen@allendav.com>
2016-06-22 18:05:42 -0700

    Sort issues in each column by priority

--- assets/js/components/issue-list/index.jsx
+++ assets/js/components/issue-list/index.jsx
@@ -2,8 +2,60 @@ import React, { Component, PropTypes } from 'react';
 import Issue from '../issue';
 
 export default class IssueList extends Component {
+	constructor( props ) {
+		super( props );
+		this.issueHasLabel = this.issueHasLabel.bind( this );
+		this.scoreIssue = this.scoreIssue.bind( this );
+		this.sortByPriority = this.sortByPriority.bind( this );
+	}
+
+	issueHasLabel( issue, name ) {
+		for ( let label of issue.labels ) {
+			if ( name === label.name ) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	scoreIssue( issue ) {
+		if ( this.issueHasLabel( issue, '[Pri] CRITICAL' ) ) {
+			return 4;
+		}
+
+		if ( this.issueHasLabel( issue, '[Pri] High' ) ) {
+			return 3;
+		}
+
+		if ( this.issueHasLabel( issue, '[Pri] Normal' ) ) {
+			return 2;
+		}
+
+		if ( this.issueHasLabel( issue, '[Pri] Low' ) ) {
+			return 1;
+		}
+
+		return 5;
+	}
+
+	sortByPriority( a, b ) {
+		const aScore = this.scoreIssue( a );
+		const bScore = this.scoreIssue( b );
+
+		if ( aScore < bScore ) {
+			return 1;
+		}
+
+		if ( aScore > bScore ) {
+			return -1;
+		}
+
+		return 0;
+	}
+
 	render() {
-		const { issues } = this.props;
+		const issues = this.props.issues.sort( this.sortByPriority );
 
 		return (
 			<div>
--- assets/js/components/issue/index.jsx
+++ assets/js/components/issue/index.jsx
@@ -1,13 +1,45 @@
 import React, { Component, PropTypes } from 'react';
 
 export default class Issue extends Component {
+
+	issueHasLabel( issue, name ) {
+		for ( let label of issue.labels ) {
+			if ( name === label.name ) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	getPriorityColor( issue ) {
+		if ( this.issueHasLabel( issue, '[Pri] CRITICAL' ) ) {
+			return "#ff0000";
+		}
+
+		if ( this.issueHasLabel( issue, '[Pri] High' ) ) {
+			return "#ff6600";
+		}
+
+		if ( this.issueHasLabel( issue, '[Pri] Normal' ) ) {
+			return "#00ff00";
+		}
+
+		if ( this.issueHasLabel( issue, '[Pri] Low' ) ) {
+			return "#ffff00";
+		}
+
+		return "#000";
+	}
+
 	render() {
 		const { issue } = this.props;
 
-		console.log( issue );
-
 		const style = {
 			backgroundColor: '#fff',
+			borderColor: this.getPriorityColor( issue ),
+			borderSize: 1,
+			borderStyle: 'solid',
 			boxShadow: '2px 2px 2px #ddd',
 			fontSize: 11,
 			marginBottom: 5,
